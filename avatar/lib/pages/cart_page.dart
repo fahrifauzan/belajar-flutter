7be5a731f7cf270641050f9608871fadd6a693ee
Fahri Fauzan <fahriifauzan32@gmail.com>
2019-01-31 10:08:48 +0700

    add metro grid  in cart page for the example

--- avatar/lib/pages/cart_page.dart
+++ avatar/lib/pages/cart_page.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 1),
+  const StaggeredTile.count(1, 2),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(1, 2),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(3, 1),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(4, 1),
+];
+
+
+List<Widget> _tiles = const <Widget>[
+  const _ImageTile('https://picsum.photos/200/300/?random'),
+  const _ImageTile('https://picsum.photos/201/300/?random'),
+  const _ImageTile('https://picsum.photos/202/300/?random'),
+  const _ImageTile('https://picsum.photos/203/300/?random'),
+  const _ImageTile('https://picsum.photos/204/300/?random'),
+  const _ImageTile('https://picsum.photos/205/300/?random'),
+  const _ImageTile('https://picsum.photos/206/300/?random'),
+  const _ImageTile('https://picsum.photos/207/300/?random'),
+  const _ImageTile('https://picsum.photos/208/300/?random'),
+  const _ImageTile('https://picsum.photos/209/300/?random'),
+];
+
 
 class CartPage extends StatefulWidget {
   @override
@@ -10,7 +39,44 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: new Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: new StaggeredGridView.count(
+          crossAxisCount: 4,
+          staggeredTiles: _staggeredTiles,
+          children: _tiles,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+        )
+      )
+    );
+  }
+}
+
+class _ImageTile extends StatelessWidget {
+  const _ImageTile(this.gridImage);
+
+  final gridImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      color: const Color(0x00000000),
+      elevation: 3.0,
+      child: new GestureDetector(
+        onTap: () {
+          print("hello");
+        },
+        child: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new NetworkImage(gridImage),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
+            )
+        ),
+      ),
     );
   }
 }
